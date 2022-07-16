8eeeaef5201114c61d5e226fa29147a6e90b07cf
erik-gh <eriktecse@gmail.com>
2022-07-16 07:32:44 -0500

    Se realizo la tabla sufragio_nacional

--- wsql/2022_07_15_163604_create_sufragio_nacional_vertical_table.php
+++ wsql/2022_07_15_163604_create_sufragio_nacional_vertical_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSufragiosTable extends Migration
+class CreateSufragioNacionalVerticalTable extends Migration
 {
     /**
      * Run the migrations.
