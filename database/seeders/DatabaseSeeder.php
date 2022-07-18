000ee49fdecc02ca84a9a15fb7d2a079934114d4
erik-gh <eriktecse@gmail.com>
2022-07-18 17:09:35 -0500

    Se modifico algunos cambios para la realizacion de migraciones

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,29 +13,46 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
+    {/*
         // \App\Models\User::factory(10)->create();
         $this->call(PerfilSeeder::class);
         $this->call(ModuloSeeder::class);
         $this->call(PerfilModuloSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(ConsultaSeeder::class);
-//        $this->call(OdpeSeeder::class);
         $this->call(TipoProcesoSeeder::class);
         $this->call(ProcesoSeeder::class);
         $this->call(ProcesoConsultaSeeder::class);
-//        $this->call(ProcesoOdpeSeeder::class);
-//        $this->call(DepartamentoSeeder::class);
-//        $this->call(ProvinciaSeeder::class);
-//        $this->call(DistritoSeeder::class);
-//        $this->call(UbigeoSeeder::class);
         $this->call(SolucionSeeder::class);
         $this->call(EtapaSeeder::class);
-//        $this->call(LocalSeeder::class);
-//        $this->call(MesaSeeder::class);
-//        $this->call(OdpeSolucionSeeder::class);
         $this->call(SobreSeeder::class);
-//        $this->call(SobreMesaSeeder::class);
+        $this->call(DocumentoSeeder::class);
+        $this->call(SolucionDocumentoSeeder::class);
+        $this->call(SufragioSeeder::class);
+        $this->call(ConsultaSufragioSeeder::class);
+        */
+        // \App\Models\User::factory(10)->create();
+        $this->call(PerfilSeeder::class);
+        $this->call(ModuloSeeder::class);
+        $this->call(PerfilModuloSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(ConsultaSeeder::class);
+        $this->call(OdpeSeeder::class);
+        $this->call(TipoProcesoSeeder::class);
+        $this->call(ProcesoSeeder::class);
+        $this->call(ProcesoConsultaSeeder::class);
+        $this->call(ProcesoOdpeSeeder::class);
+        $this->call(DepartamentoSeeder::class);
+        $this->call(ProvinciaSeeder::class);
+        $this->call(DistritoSeeder::class);
+        $this->call(UbigeoSeeder::class);
+        $this->call(SolucionSeeder::class);
+        $this->call(EtapaSeeder::class);
+        $this->call(LocalSeeder::class);
+        $this->call(MesaSeeder::class);
+        $this->call(OdpeSolucionSeeder::class);
+        $this->call(SobreSeeder::class);
+        $this->call(SobreMesaSeeder::class);
         $this->call(DocumentoSeeder::class);
         $this->call(SolucionDocumentoSeeder::class);
         $this->call(SufragioSeeder::class);
--- database/seeders/LocalSeeder.php
+++ database/seeders/LocalSeeder.php
@@ -20,42 +20,42 @@ class LocalSeeder extends Seeder
                 'nombre_local' => 'IE 18288 ISABEL LYNCH DE RUBIO',
                 'direccion_local' => 'JR AYACUCHO 870',
                 'estado' => 1,
-                'id_distrito' => 1,
+                'id_ubigeo' => 1,
             ],//1
             [
                 'cod_local' => '7805',
                 'nombre_local' => 'IEIPSM Nº 16194',
                 'direccion_local' => 'JR ICA SN CUADRA 12',
                 'estado' => 1,
-                'id_distrito' => 2,
+                'id_ubigeo' => 2,
             ],//2
             [
                 'cod_local' => '0150',
                 'nombre_local' => 'IE PEDRO PABLO ATUSPARIA',
                 'direccion_local' => 'AV BOLOGNESI 116',
                 'estado' => 1,
-                'id_distrito' => 3,
+                'id_ubigeo' => 3,
             ],//3
             [
                 'cod_local' => '0155',
                 'nombre_local' => 'IE GRAN UNIDAD ESCOLAR MARISCAL TORIBIO DE LUZURIAGA',
                 'direccion_local' => 'AV CENTENARIO 998',
                 'estado' => 1,
-                'id_distrito' => 4,
+                'id_ubigeo' => 4,
             ],//4
             [
                 'cod_local' => '8131',
                 'nombre_local' => '88101 CESAR ABRAHAM VALLEJO MENDOZA',
                 'direccion_local' => 'CALLE LOS PINOS SN',
                 'estado' => 1,
-                'id_distrito' => 5,
+                'id_ubigeo' => 5,
             ],//5
             [
                 'cod_local' => '0216',
                 'nombre_local' => 'IE 88110 NUESTRA SEÑORA DE FATIMA',
                 'direccion_local' => 'AV VILLARREAL SN',
                 'estado' => 1,
-                'id_distrito' => 6,
+                'id_ubigeo' => 6,
             ],//6
         ];
         DB::table('locales')->insert($datos);
