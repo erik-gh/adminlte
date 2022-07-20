85cc67cedd230a608ba5f50a9bd2e62d8ff4b439
erik-gh <eriktecse@gmail.com>
2022-07-20 00:29:22 -0500

    Update 2022_07_20_032000_create_recepcion_documentos_table.php

--- database/migrations/2022_07_20_032000_create_recepcion_documentos_table.php
+++ database/migrations/2022_07_20_032000_create_recepcion_documentos_table.php
@@ -13,8 +13,18 @@ class CreateRecepcionDocumentosTable extends Migration
      */
     public function up()
     {
+        /* Esta tabla es de prueba para ingresar por mientras  los documentos */
         Schema::create('recepcion_documentos', function (Blueprint $table) {
             $table->id();
+            $table->integer('id_proceso');
+            $table->integer('id_solucion');
+            $table->integer('id_odpe');
+            $table->integer('id_ubigeo');
+            $table->integer('id_consulta');
+            $table->integer('id_sobre');
+            $table->integer('id_sufragio');
+            $table->integer('id_documento');
+            $table->integer('id_mesa');
             $table->timestamps();
         });
     }
