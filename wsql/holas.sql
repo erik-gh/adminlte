f8800c4c2ea7ed99f368c7a130799c9a74bdf841
erik-gh <eriktecse@gmail.com>
2022-07-25 10:13:35 -0500

    Update holas.sql

--- wsql/holas.sql
+++ wsql/holas.sql
@@ -1,4 +1,6 @@
-select * from mesas; -- 80940
+select *
+from mesas;
+-- 80940
 /* Sacando las mesas por solucionTecnologia - codigoConsulta */
 
 SELECT N_MESA_MADRE, COD_ST, COD_CONSULTA
@@ -44,13 +46,14 @@ GROUP BY N_MESA_MADRE, c.consulta, c.descripcion, s.codigo, s.descripcion;
 SELECT N_MESA_MADRE, c.consulta, c.descripcion, s.id, s.descripcion -- ,COUNT(codigo) AS cant_act
 FROM (SELECT N_MESA_MADRE, COD_ST, COD_CONSULTA
       FROM sufragio_nacional_vertical
-      WHERE COD_ST = 1 AND N_MESA_MADRE = '000001'
+      WHERE COD_ST = 1
+        AND N_MESA_MADRE = '000001'
       GROUP BY N_MESA_MADRE, COD_ST, COD_CONSULTA
       ORDER BY N_MESA_MADRE) snv
          INNER JOIN consultas c ON snv.COD_CONSULTA = c.consulta
          INNER JOIN consulta_sufragios cs on c.id = cs.id_consulta
          INNER JOIN sufragios s ON cs.id_sufragio = s.id
-         INNER JOIN mesas m  ON snv.N_MESA_MADRE = m.nro_mesa
+         INNER JOIN mesas m ON snv.N_MESA_MADRE = m.nro_mesa
          INNER JOIN sobre_mesas sm on m.id = sm.id_mesa
          INNER JOIN sobres sb ON sm.id_sobre = sb.id
          INNER JOIN solucion_documentos sd on sb.id = sd.id_sobre
@@ -76,7 +79,8 @@ FROM solucion_documentos;
 
 SELECT *
 FROM sufragios;
-SELECT * FROM sobre_mesas;
+SELECT *
+FROM sobre_mesas;
 SELECT *
 FROM consultas c
          INNER JOIN sufragios s on c.estado = s.estado;
@@ -86,22 +90,28 @@ SELECT *
 FROM sufragios;
 /* Realizacion de query para poder ver las consultas */
 SELECT DISTINCT c.id, c.descripcion
-                    FROM ubigeo_consultas uc
-                             INNER JOIN consultas c ON c.id = uc.id_consulta
-                             INNER JOIN ubigeos u ON uc.id_ubigeo = u.id
-                    WHERE uc.id_proceso = 1
-                      AND u.id_odpe = 1
-                      AND u.id_departamento = 3
-                    ORDER BY c.id;
+FROM ubigeo_consultas uc
+         INNER JOIN consultas c ON c.id = uc.id_consulta
+         INNER JOIN ubigeos u ON uc.id_ubigeo = u.id
+WHERE uc.id_proceso = 1
+  AND u.id_odpe = 1
+  AND u.id_departamento = 3
+ORDER BY c.id;
 
 SELECT o.id, o.nombre_odpe
 FROM procesos p
-    INNER JOIN ubigeo_consultas uc on p.id = uc.id_proceso
-    INNER JOIN ubigeos u on uc.id_ubigeo = u.id
-    INNER JOIN odpes o on u.id_odpe = o.id
+         INNER JOIN ubigeo_consultas uc on p.id = uc.id_proceso
+         INNER JOIN ubigeos u on uc.id_ubigeo = u.id
+         INNER JOIN odpes o on u.id_odpe = o.id
 WHERE p.id = 1
 group by o.id, o.nombre_odpe
 order by o.id, o.nombre_odpe;
+
+SELECT s.id, s.solucion_tecnologica
+FROM odpe_soluciones os
+         INNER JOIN soluciones s on os.id_solucion = s.id
+WHERE os.id_odpe = 10
+ORDER BY solucion_tecnologica;
 
 
 
